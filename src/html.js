c3a3c63bfc26a08aa684580681363917ca098273
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-09-17 22:33:36 +0900

    wip

--- src/html.js
+++ src/html.js
@@ -1,12 +1,30 @@
 import { core } from "./core.js"
 
-function keyParser(key) {
-	
-}
+/*
+	html instance constructor
+
+	workflow:
+	1. parse joined template literal string, search where the key comes from
+	2. 
+*/
 
 function html(strings, ...keys) {
+	// workflow[1]
 	for(let i = 0; i < keys.length; i++) {
 		if((typeof keys[i]) === "function") {
+			const registry = Object.create(null);
+			/*
+				registry = {
+					tempFnString: "$ => ({...})" | "function($) { return ... }"
+					tempFnType: "arrow" | "standard",
+					tempFnArg: "$" or what else
+					tempHandleMap: {
+						count: 0
+					}
+				}
+			*/
+			registry.templateString = keys[i].toString();
+
 			core.functionRegistry[core.functionRegistryLength] = {
 				templateString: keys[i].toString(),
 			};
