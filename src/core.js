8d213eab3faede805b202d6bccfea4c17659502c
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-09-18 15:27:52 +0900

    wip

--- src/core.js
+++ src/core.js
@@ -14,39 +14,6 @@ const core = {
 	keyCache: Object.create(null),
 	funcCache: Object.create(null),
 	attributeRegistry: Object.create(null),
-	init(str, ...key) {
-		// parse string
-
-		if(typeof key[0] === "function") {
-			const funcStr = key[0].toString();
-			const funcInfo = {
-				body: funcStr.match(),
-			}
-			// parse function
-			let objectRoot = {
-				this: {
-					_count: 0,
-					style: {
-						"*": {
-							color: "red"
-						}
-					},
-					src: "app.com"
-				},
-				prop: {
-					"hx-swap": {
-						avatarURL: "google.png"
-					}
-				}
-			}
-			const funcPrototype = key[0](objectRoot);
-			console.log(funcPrototype);
-			return funcPrototype;
-		} else if(typeof key[0] === "function") {
-			console.log(key[0]);
-			return {};
-		}
-	},
 
 	config: {},
 	compile: {},
