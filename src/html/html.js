d06d1344518489ed9101115e3b984e5d0c2e991a
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-09-18 21:28:39 +0900

    wip

--- src/html/html.js
+++ src/html/html.js
@@ -8,29 +8,43 @@ import { core } from "../core.js"
 	2. 
 */
 const parseBuffer = {
-	templateString: "",
-	templateDOM: Object.create(null),
-	keyLocation: [],
-	HTMLParser: new DOMParser()
+	HTMLParser: new DOMParser(),
+	resetBuffer() {
+		Object.assign(this, {
+			scope: {
+				"{": false,
+			},
+			endIndex: -1,
+			unifiedString: "",
+			templateString: "",
+			templateDOM: Object.create(null),
+			keyLocation: [],
+		})
+	}
 };
+
+parseBuffer.resetBuffer();
 
 function html(strings, ...keys) {
 	// parse string
-	for(let i = 0; i < strings.length; i++) {
-		parseBuffer.keyLocation.push(strings[i].length);
-		if(/{/.test(strings[i])) {
-			parseBuffer.templateString += strings[i].replace(/[\s\S]*{/, " ");
-		} else if(/}/.test(strings[i])) {
-			parseBuffer.templateString += strings[i].replace(/}[\s\S]*/, " ");
+	for(let i = strings.length - 1; -1 < i; i--) {
+		if(/}/.test(strings[i])) {
+			parseBuffer.endIndex = i;
 			break;
-		} else {
-			parseBuffer.templateString += strings[i];
 		};
 	};
-	parseBuffer.templateDOM = parseBuffer.HTMLParser.parseFromString(parseBuffer.templateString, "application/xml");
-	console.log(parseBuffer.templateDOM);
-
-	for(let i = 0; i < keys.length; i++) {
+	if(parseBuffer.endIndex === -1) {
+		throw new Error("Parse error");
+	};
+	console.log(parseBuffer.endIndex);
+	for(let i = 0; i < parseBuffer.endIndex; i++) {
+		parseBuffer.keyLocation.push(strings[i].length);
+		if(/{/.test(strings[i])) {
+			parseBuffer.scope["{"] = true;
+			parseBuffer.templateString += strings[i].replace(/[\s\S]*{/, " ");
+		} else if(parseBuffer.scope["{"]) {
+			parseBuffer.templateString += strings[i];
+		};
 		if((typeof keys[i]) === "function") {
 			const registry = Object.create(null);
 			/*
@@ -55,12 +69,10 @@ function html(strings, ...keys) {
 
 		}
 	};
+	parseBuffer.templateDOM = parseBuffer.HTMLParser.parseFromString(parseBuffer.templateString, "text/html");
+	console.log(parseBuffer.templateDOM.body);
 	// reset buffer
-	Object.assign(parseBuffer, {
-		templateString: "",
-		templateDOM: Object.create(null),
-		keyLocation: [],
-	});
+	parseBuffer.resetBuffer();
 };
 
 html.getReservedKey = [
