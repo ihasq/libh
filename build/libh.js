ab3437c4036c51eeac59dc811e994c1b9615db38
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-09-22 19:40:08 +0900

    wip

--- build/libh.js
+++ build/libh.js
@@ -15,6 +15,24 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __accessCheck = (obj, member, msg) => {
+  if (!member.has(obj))
+    throw TypeError("Cannot " + msg);
+};
+var __privateGet = (obj, member, getter) => {
+  __accessCheck(obj, member, "read from private field");
+  return getter ? getter.call(obj) : member.get(obj);
+};
+var __privateAdd = (obj, member, value) => {
+  if (member.has(obj))
+    throw TypeError("Cannot add the same private member more than once");
+  member instanceof WeakSet ? member.add(obj) : member.set(obj, value);
+};
+var __privateSet = (obj, member, value, setter) => {
+  __accessCheck(obj, member, "write to private field");
+  setter ? setter.call(obj, value) : member.set(obj, value);
+  return value;
+};
 
 // src/libh.js
 var libh_exports = {};
@@ -30,6 +48,88 @@ module.exports = __toCommonJS(libh_exports);
 // src/core/getKeyIdentifier.js
 var generateInstanceId = function() {
   return (Math.floor(Math.random() * (32 ** 6 - 32 ** 5 - 1)) + 32 ** 5).toString(32);
+};
+
+// src/util/TurboArray.js
+var _private;
+var TurboArray = class {
+  constructor() {
+    __privateAdd(this, _private, void 0);
+    __privateSet(this, _private, {
+      table: /* @__PURE__ */ Object.create(null),
+      length: 0
+    });
+  }
+  at(arrayIndex) {
+    if (__privateGet(this, _private).length) {
+      return __privateGet(this, _private).table[arrayIndex % __privateGet(this, _private).length];
+    } else {
+      return void 0;
+    }
+  }
+  push(property) {
+    __privateGet(this, _private).table[__privateGet(this, _private).length] = property;
+    __privateGet(this, _private).length++;
+  }
+  flush() {
+    __privateGet(this, _private).length = 0;
+  }
+  forEach(callbackFn) {
+  }
+  get length() {
+    return __privateGet(this, _private).length;
+  }
+};
+_private = new WeakMap();
+
+// src/core/frameloop.js
+var frameloop = {
+  isActive: false,
+  rafID: "",
+  ignite() {
+    if (!this.isActive) {
+      this.rafID = window.requestAnimationFrame(this.run);
+      this.isActive = true;
+    }
+    ;
+  },
+  cancel() {
+    window.cancelAnimationFrame(this.rafID);
+    this.rafID = "";
+    this.isActive = false;
+  },
+  func: {
+    stack: new TurboArray(),
+    pendedDiff: /* @__PURE__ */ Object.create(null)
+  },
+  task: {
+    stack: new TurboArray()
+  },
+  registerNewFunction(keyFn) {
+    this.func.stack.push({
+      funcBody: keyFn,
+      result: null
+    });
+  },
+  pushNewTask(taskFn) {
+    this.task.stack.push(taskFn);
+  },
+  run() {
+    if (frameloop.func.stack.length !== 0) {
+      for (let i = 0; i < frameloop.func.stack.length; i++) {
+        if (frameloop.func.stack.at(i).result !== frameloop.func.stack.at(i).funcBody()) {
+        }
+      }
+    }
+    if (frameloop.task.stack.length !== 0) {
+      for (let i = 0; i < frameloop.task.stack.length; i++) {
+        frameloop.task.stack.at(i)();
+      }
+    }
+    ;
+    frameloop.task.stack.flush();
+    window.requestAnimationFrame(frameloop.run);
+  }
 };
 
 // src/html/html.js
@@ -49,8 +149,8 @@ function createHTMLInstance(instanceId, strings, keys) {
         case "function":
           if (keys[index].constructor.name !== "Function") {
             throw new Error("Can not use async function");
+          } else {
           }
-          ;
           break;
         case "object":
           throw new Error("dumb ass");
@@ -66,7 +166,7 @@ function createHTMLInstance(instanceId, strings, keys) {
     buffer.keyMap.slice(buffer.keyMap.indexOf("{") + 1, buffer.keyMap.lastIndexOf("}")),
     "text/html"
   ).body;
-  console.log(TEMPLATE);
+  console.dir(TEMPLATE.children);
   parseBuffer.registry[instanceId] = Object.assign(buffer, {
     instanceId,
     keys: {}
