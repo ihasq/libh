f0aaa7e532c5657174db35daea27be320f0eaeb2
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-09-23 21:16:07 +0900

    wip

--- build/libh.esm.js
+++ build/libh.esm.js
@@ -1,38 +1,44 @@
-// src/core.js
-function generateInstanceId() {
-  return (Math.floor(Math.random() * (32 ** 6 - 32 ** 5 - 1)) + 32 ** 5).toString(32);
-}
-
 // src/html.js
-var parseBuffer = {
+var PARSE_BUFFER = {
   registry: /* @__PURE__ */ Object.create(null),
   HTMLParser: new DOMParser()
 };
 function functionParser(fnBody) {
   const TEMPLATE_STRING = "" + fnBody;
   const FUNC_TYPE = fnBody.hasOwnProperty("prototype") ? "normal" : fnBody.name ? "normal" : "arrow";
-  console.log(FUNC_TYPE);
+  let FUNC_ARG = "";
+  let FUNC_NAME;
+  if (FUNC_TYPE === "normal") {
+    if (fnBody.name) {
+      FUNC_ARG = TEMPLATE_STRING.slice(TEMPLATE_STRING.indexOf(`function ${fnBody.name}`));
+    } else {
+    }
+    FUNC_ARG = TEMPLATE_STRING.slice(TEMPLATE_STRING.indexOf("function"));
+    FUNC_NAME = fnBody.name;
+  } else {
+  }
+  console.log(FUNC_NAME);
   return {
     TEMPLATE_STRING,
     FUNC_TYPE
   };
 }
 function createHTMLInstance(instanceId, strings, keys) {
-  const buffer = {
+  const BUFFER = {
     keyMap: "",
     funcList: []
   };
   for (let index = 0; index < strings.length; index++) {
-    buffer.keyMap += strings[index];
+    BUFFER.keyMap += strings[index];
     if (index + 1 !== strings.length) {
-      buffer.keyMap += ` \${${instanceId}:${index}} `;
+      BUFFER.keyMap += ` \${${instanceId}:${index}} `;
       switch (typeof keys[index]) {
         case "function":
           if (keys[index].constructor.name !== "Function") {
             throw new Error("Can not use async function");
           } else {
-            buffer.funcList.push(functionParser(keys[index]));
-            console.log(buffer.funcList[index].TEMPLATE_STRING);
+            BUFFER.funcList.push(functionParser(keys[index]));
+            console.log(BUFFER.funcList[index].TEMPLATE_STRING);
           }
           break;
         case "object":
@@ -43,29 +49,33 @@ function createHTMLInstance(instanceId, strings, keys) {
     }
   }
   ;
-  console.log(buffer.keyMap);
+  console.log(BUFFER.keyMap);
   const SELECTOR = new RegExp(` \\$\\{${instanceId}:[0-9]\\} `, "g");
-  const TEMPLATE = parseBuffer.HTMLParser.parseFromString(
-    buffer.keyMap.slice(buffer.keyMap.indexOf("{") + 1, buffer.keyMap.lastIndexOf("}")),
+  const TEMPLATE = PARSE_BUFFER.HTMLParser.parseFromString(
+    BUFFER.keyMap.slice(BUFFER.keyMap.indexOf("{") + 1, BUFFER.keyMap.lastIndexOf("}")),
     "text/html"
   ).body;
   console.dir(TEMPLATE.children);
-  parseBuffer.registry[instanceId] = Object.assign(buffer, {
+  PARSE_BUFFER.registry[instanceId] = Object.assign(BUFFER, {
     instanceId,
     keys: {}
   });
 }
 function html(strings, ...keys) {
-  createHTMLInstance(generateInstanceId(), strings, keys);
+  createHTMLInstance(crypto.randomUUID(), strings, keys);
   return;
 }
 html.getReservedKey = [
+  "global",
   "shared",
   "prop",
   "this",
   "static",
   "method",
-  "meta"
+  "meta",
+  "event",
+  "onclick",
+  "oneffect"
 ];
 
 // src/css.js
