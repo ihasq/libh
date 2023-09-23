55eb0ef63d47bd28454ae19d4b3dd03ab179d8e2
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-09-24 07:55:28 +0900

    wip

--- src/core.js
+++ src/core.js
@@ -51,14 +51,14 @@ const STATIC_UUID = {
 	index: 0,
 	reset() {
 		for(let i = 0; i < 256; i++) {
-			this.registry[i] = crypto.randomUUID();
+			this.registry[i] = window.crypto.randomUUID();
 		};
-		this.index = 0;
+		this.index = -1;
 	}
 };
 
 function getStaticUUID() {
-	if(STATIC_UUID.index === 255) {
+	if(STATIC_UUID.index === 254) {
 		setTimeout(STATIC_UUID.reset, 0);	
 	};
 	STATIC_UUID.index++;
