590fac15c858bf052be278e9f14195b938792779
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-10-03 00:02:12 +0900

    wip

--- build/libh.cjs
+++ build/libh.cjs
@@ -15,24 +15,6 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-var __accessCheck = (obj, member, msg) => {
-  if (!member.has(obj))
-    throw TypeError("Cannot " + msg);
-};
-var __privateGet = (obj, member, getter) => {
-  __accessCheck(obj, member, "read from private field");
-  return getter ? getter.call(obj) : member.get(obj);
-};
-var __privateAdd = (obj, member, value) => {
-  if (member.has(obj))
-    throw TypeError("Cannot add the same private member more than once");
-  member instanceof WeakSet ? member.add(obj) : member.set(obj, value);
-};
-var __privateSet = (obj, member, value, setter) => {
-  __accessCheck(obj, member, "write to private field");
-  setter ? setter.call(obj, value) : member.set(obj, value);
-  return value;
-};
 var libh_exports = {};
 __export(libh_exports, {
   html: () => html
@@ -43,7 +25,7 @@ module.exports = __toCommonJS(libh_exports);
     BASE_CLASS: Node,
     TARGET: "appendChild",
     ADDITION: function() {
-      const HAS_LIBH_FLAG = arguments[0].FLAG === "LIBH_INSTANCE";
+      const HAS_LIBH_FLAG = arguments[0].constructor.name === "String" && arguments[0].FLAG === "LIBH_INSTANCE";
       const LIBH_ELEMENT_NODE = arguments[0].getAsNode;
       arguments[arguments.length - 1].apply(
         this,
@@ -80,132 +62,136 @@ const UTIL = {
   },
   HTMLParser: new DOMParser()
 };
-const BANNED_PROPERTY = [
-  "prop",
-  "super",
-  "__proto__",
-  "__defineGetter__",
-  "__defineSetter__",
-  "__lookupGetter__",
-  "__lookupSetter__"
-];
-const HTML_FLAG = {
-  "enable-node-return": false
+const QUERY = {
+  BANNED_PROPERTY: [
+    "prop",
+    "super",
+    "__proto__",
+    "__defineGetter__",
+    "__defineSetter__",
+    "__lookupGetter__",
+    "__lookupSetter__"
+  ],
+  RESERVED_KEY: [
+    "global",
+    "shared",
+    "prop",
+    "this",
+    "static",
+    "method",
+    "meta",
+    "event",
+    "onclick",
+    "onchange"
+  ]
 };
-function html(STRINGS, ...KEYS) {
-  var _buffer, _a;
-  return new (_a = class extends String {
-    constructor({ RENDER_TARGET_NONCE, STRINGS: STRINGS2, KEYS: KEYS2 }) {
-      super(`<span id=${RENDER_TARGET_NONCE} hidden></span>`);
-      __privateAdd(this, _buffer, void 0);
-      __privateSet(this, _buffer, {
-        INSTANCE_UUID: crypto.randomUUID(),
-        RENDER_TARGET_NONCE,
-        keyMap: "",
-        funcList: [],
-        portConfig: /* @__PURE__ */ Object.create(null),
-        proxyRegistry: /* @__PURE__ */ Object.create(null),
-        proxyHandleTemplate: {
-          get(target, prop) {
-            console.dir(target);
-            if (prop in target) {
-              console.log("already has");
-              return target[prop];
-            } else {
-              const proxyRef = crypto.randomUUID();
-              console.log("proxy created");
-              target[prop] = /* @__PURE__ */ Object.create(null);
-              return new Proxy(target[prop], this);
-            }
-          },
-          set(target, prop, value) {
-            target[prop] = value;
+const BUFFER = {
+  flags: {
+    "enable-node-return": false
+  },
+  elementRegistry: /* @__PURE__ */ Object.create(null),
+  createElement() {
+    const INSTANCE_UUID = crypto.randomUUID();
+    this.elementRegistry[INSTANCE_UUID] = {
+      INSTANCE_UUID,
+      keyMap: "",
+      funcList: [],
+      portConfig: /* @__PURE__ */ Object.create(null),
+      proxyRegistry: /* @__PURE__ */ Object.create(null),
+      proxyHandleTemplate: {
+        get(target, prop) {
+          console.dir(target);
+          if (prop in target) {
+            console.log("already has");
+            return target[prop];
+          } else {
+            const proxyRef = crypto.randomUUID();
+            console.log("proxy created");
+            target[prop] = /* @__PURE__ */ Object.create(null);
+            return new Proxy(target[prop], this);
           }
         },
-        elementProperty: {
-          globalVariable: /* @__PURE__ */ Object.create(null),
-          propReference: null
+        set(target, prop, value) {
+          target[prop] = value;
         }
-      });
-      for (let keyIndex = 0; keyIndex < STRINGS2.length; keyIndex++) {
-        __privateGet(this, _buffer).keyMap += STRINGS2[keyIndex];
-        if (keyIndex + 1 !== STRINGS2.length) {
-          switch (typeof KEYS2[keyIndex]) {
-            case "function":
-              __privateGet(this, _buffer).keyMap += ` \${${__privateGet(this, _buffer).INSTANCE_UUID}:${keyIndex}} `;
-              if (KEYS2[keyIndex].constructor.name !== "Function") {
-                throw new Error("Can not use async function");
-              } else {
-                const typeMap = UTIL.getDeepCopy(KEYS2[keyIndex](new Proxy({}, __privateGet(this, _buffer).proxyHandleTemplate)));
-                const resultBuffer = KEYS2[keyIndex](typeMap);
-                resultBuffer.onclick();
-                console.log(typeMap);
-              }
-              ;
-              break;
-            case "object":
-              __privateGet(this, _buffer).keyMap += ` \${${__privateGet(this, _buffer).INSTANCE_UUID}:${keyIndex}} `;
-              __privateGet(this, _buffer).portConfig = KEYS2[keyIndex];
-              for (let banIndex = 0; banIndex < BANNED_PROPERTY.length; banIndex++) {
-                delete __privateGet(this, _buffer).portConfig[BANNED_PROPERTY[banIndex]];
-              }
-              ;
-              __privateGet(this, _buffer).portConfig.onclick(__privateGet(this, _buffer).portConfig);
-              console.log(__privateGet(this, _buffer).portConfig);
-              break;
-            default:
-              __privateGet(this, _buffer).keyMap += KEYS2[keyIndex];
+      },
+      elementProperty: {
+        globalVariable: /* @__PURE__ */ Object.create(null),
+        propReference: null
+      },
+      createRenderPath() {
+        return {
+          NONCE: crypto.randomUUID()
+        };
+      }
+    };
+    return this.elementRegistry[INSTANCE_UUID];
+  }
+};
+function html(STRINGS, ...KEYS) {
+  const BUFFER_PATH = BUFFER.createElement();
+  const RENDER_PATH = BUFFER_PATH.createRenderPath();
+  for (let keyIndex = 0; keyIndex < STRINGS.length; keyIndex++) {
+    BUFFER_PATH.keyMap += STRINGS[keyIndex];
+    if (keyIndex + 1 !== STRINGS.length) {
+      switch (typeof KEYS[keyIndex]) {
+        case "function":
+          BUFFER_PATH.keyMap += ` \${${BUFFER_PATH.INSTANCE_UUID}:${keyIndex}} `;
+          if (KEYS[keyIndex].constructor.name !== "Function") {
+            throw new Error("Can not use async function");
+          } else {
+            const typeMap = UTIL.getDeepCopy(KEYS[keyIndex](new Proxy({}, BUFFER_PATH.proxyHandleTemplate)));
+            const resultBuffer = KEYS[keyIndex](typeMap);
+            resultBuffer.onclick();
+            console.log(typeMap);
           }
           ;
-        }
-        ;
+          break;
+        case "object":
+          BUFFER_PATH.keyMap += ` \${${BUFFER_PATH.INSTANCE_UUID}:${keyIndex}} `;
+          BUFFER_PATH.portConfig = KEYS[keyIndex];
+          for (let banIndex = 0; banIndex < QUERY.BANNED_PROPERTY.length; banIndex++) {
+            delete BUFFER_PATH.portConfig[QUERY.BANNED_PROPERTY[banIndex]];
+          }
+          ;
+          BUFFER_PATH.portConfig.onclick(BUFFER_PATH.portConfig);
+          console.log(BUFFER_PATH.portConfig);
+          break;
+        default:
+          BUFFER_PATH.keyMap += KEYS[keyIndex];
       }
       ;
-      console.log(__privateGet(this, _buffer).keyMap);
-      setTimeout(function() {
-        const TARGET = document.getElementById(RENDER_TARGET_NONCE);
-        if (!TARGET) {
-          console.log(`instance created: ${__privateGet(this, _buffer).INSTANCE_UUID}`);
-          __privateGet(this, _buffer).returnObject.flag = void 0;
-        } else {
-          console.log("html appended");
-          TARGET.removeAttribute("id");
-          TARGET.removeAttribute("hidden");
-        }
-        ;
-      }, 0);
     }
+    ;
+  }
+  ;
+  setTimeout(function() {
+    const TARGET = document.getElementById(RENDER_PATH.NONCE);
+    if (!TARGET) {
+      console.log(`instance created: ${BUFFER_PATH.INSTANCE_UUID}`);
+      BUFFER_PATH.returnObject.flag = void 0;
+    } else {
+      console.log("html appended");
+      TARGET.removeAttribute("id");
+      TARGET.removeAttribute("hidden");
+    }
+    ;
+  }, 0);
+  return Object.assign(new String(`<span id=${RENDER_PATH.NONCE} hidden></span>`), {
     get FLAG() {
       return "LIBH_INSTANCE";
-    }
+    },
     get getAsNode() {
       const RETURN_NODE = document.createElement("span");
       RETURN_NODE.innerText = Date.now();
-      RETURN_NODE.id = __privateGet(this, _buffer).RENDER_TARGET_NONCE;
+      RETURN_NODE.id = RENDER_PATH.NONCE;
       return RETURN_NODE;
     }
-  }, _buffer = new WeakMap(), _a)(
-    {
-      RENDER_TARGET_NONCE: crypto.randomUUID(),
-      STRINGS,
-      KEYS
-    }
-  );
+  });
 }
 Object.assign(html, {
   get reservedKey() {
-    return [
-      "global",
-      "shared",
-      "prop",
-      "this",
-      "static",
-      "method",
-      "meta",
-      "event",
-      "onclick",
-      "onchange"
-    ];
+    return QUERY.RESERVED_KEY;
   },
   get info() {
     return {
@@ -213,14 +199,14 @@ Object.assign(html, {
       "cdn": "npm",
       "module": "html",
       "version": "0.0.16",
-      "available-flags": Object.keys(HTML_FLAG)
+      "available-flags": Object.keys(BUFFER.flags)
     };
   }
 });
 html.flag = function(...flag) {
   for (const FLAG_INDEX of flag) {
-    if (FLAG_INDEX in HTML_FLAG && !HTML_FLAG[FLAG_INDEX]) {
-      HTML_FLAG[FLAG_INDEX] = true;
+    if (FLAG_INDEX in BUFFER.flags && !BUFFER.flags[FLAG_INDEX]) {
+      BUFFER.flags[FLAG_INDEX] = true;
     }
     ;
   }
@@ -228,9 +214,9 @@ html.flag = function(...flag) {
 };
 Object.assign(html.flag, {
   get list() {
-    return Object.keys(HTML_FLAG);
+    return Object.keys(BUFFER.flags);
   },
   get state() {
-    return JSON.parse(JSON.stringify(HTML_FLAG));
+    return JSON.parse(JSON.stringify(BUFFER.flags));
   }
 });
